f6c86db28bc44d56d0665dd34ee8eabeffe77427
Vladimir Shibakov <shibakov93@mail.ru>
2023-10-14 23:01:59 +0300

    Finished lw2

--- lw2/16.cpp
+++ lw2/16.cpp
@@ -14,25 +14,61 @@ Visual Studio Code
 */
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <deque>
 #include <algorithm>
 #include <cmath>
 
 struct Tree
 {
-    int player;
+    std::string playerName;
+    int playerRate;
     Tree *left = 0;
     Tree *right = 0;
 };
 
-void insertIntoTree(Tree *&node, std::deque<int> players, int level)
+struct Player
+{
+    std::string name;
+    int rate;
+};
+
+void readFromFile(std::string &fileName, std::deque<Player> &players)
+{
+    if (fileName.find_last_of('.') == std::string::npos)
+    {
+        fileName.append(".txt");
+    }
+    std::ifstream file;
+    file.open(fileName);
+
+    if (file.is_open())
+    {
+        std::string player;
+        int rating;
+
+        while (file >> player >> rating)
+        {
+            players.push_back({player, rating});
+        }
+
+        file.close();
+    }
+    else
+    {
+        std::cout << "Couldn't open the file " << fileName << ".\n";
+    }
+}
+
+void insertIntoTree(Tree *&node, std::deque<Player> players, int level)
 {
     level++;
 
-    std::deque<int> temp;
+    std::deque<Player> temp;
     std::copy(players.begin(), players.end(), std::back_inserter(temp));
 
-    std::deque<int> strongestPlayers;
+    std::deque<Player> strongestPlayers;
     for (int i = 0; i < std::pow(2, level); i++)
     {
         strongestPlayers.push_back(temp.back());
@@ -44,10 +80,11 @@ void insertIntoTree(Tree *&node, std::deque<int> players, int level)
     if (!node)
     {
         node = new Tree;
-        node->player = strongestPlayers.front();
+        node->playerName = strongestPlayers.front().name;
+        node->playerRate = strongestPlayers.front().rate;
     }
 
-    while (strongestPlayers.front() != node->player)
+    while (strongestPlayers.front().rate != node->playerRate)
     {
         strongestPlayers.pop_front();
         strongestPlayers.pop_back();
@@ -55,8 +92,10 @@ void insertIntoTree(Tree *&node, std::deque<int> players, int level)
 
     node->left = new Tree;
     node->right = new Tree;
-    node->left->player = strongestPlayers.front();
-    node->right->player = strongestPlayers.back();
+    node->left->playerName = strongestPlayers.front().name;
+    node->left->playerRate = strongestPlayers.front().rate;
+    node->right->playerName = strongestPlayers.back().name;
+    node->right->playerRate = strongestPlayers.back().rate;
 
     if (treeNotFinished)
     {
@@ -74,28 +113,58 @@ int getHeight(Tree *node)
     return 1 + getHeight(node->left);
 }
 
-void printAndDeleteTree(Tree *&node, int level)
+void printAndDeleteTree(std::ofstream &output, Tree *&node, int level)
 {
     if (node)
     {
-        printAndDeleteTree(node->left, level - 1);
+        printAndDeleteTree(output, node->left, level - 1);
         for (int i = 0; i < level; i++)
         {
-            std::cout << "     ";
+            output << "          ";
         }
-        std::cout << node->player << "\n";
-        printAndDeleteTree(node->right, level - 1);
+        output << node->playerName << " " << node->playerRate << "\n";
+        printAndDeleteTree(output, node->right, level - 1);
         delete node;
     }
 }
 
+void printToFile(std::string &fileName, Tree *&node, int level)
+{
+    if (fileName.find_last_of('.') == std::string::npos)
+    {
+        fileName.append(".txt");
+    }
+    std::ofstream file(fileName, std::ios_base::out | std::ios_base::trunc);
+
+    if (file.is_open())
+    {
+        printAndDeleteTree(file, node, level - 1);
+        file.close();
+    }
+    else
+    {
+        std::cout << "Failed to open the file " << fileName << "\n";
+    }
+}
+
+bool lessRating(const Player &x, const Player &y)
+{
+    return x.rate < y.rate;
+}
+
 int main()
 {
-    std::deque<int> allPlayers{1, 1, 1, 1, 4, 4, 4, 5};
+    std::string input;
+    std::string output;
+    std::deque<Player> allPlayers;
     Tree *root = 0;
     int treeLevel = 0;
 
-    std::sort(allPlayers.begin(), allPlayers.end());
+    std::cout << "\nEnter input and output file names: ";
+    std::cin >> input >> output;
+
+    readFromFile(input, allPlayers);
+    std::sort(allPlayers.begin(), allPlayers.end(), lessRating);
 
     if (allPlayers.size() > 1)
     {
@@ -103,5 +172,7 @@ int main()
     }
 
     int treeHeight = getHeight(root);
-    printAndDeleteTree(root, treeHeight - 1);
+    printToFile(output, root, treeHeight);
+
+    std::cout << "\nDone!\n";
 }
